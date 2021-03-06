c58e50f34a9759c6fc9f1777a65f58433d6e21f6
minsunny5 <minsunny5@gmail.com>
2021-03-06 16:06:28 +0900

    Check the list is empty & fix memory leak.
    Before removing elements, check the list is empty or not.
    At the destructor, remove all element to fix memory leak.`

--- devMS/week1/implementation/singly-linked-list/main.cpp
+++ devMS/week1/implementation/singly-linked-list/main.cpp
@@ -6,11 +6,12 @@ int main()
 {
 	LinkedList<int> list;
 	list.insertLast(1); list.printList(); cout << "insertLast 1" << endl;
-  list.insertFront(0); list.printList(); cout << "insertFront 0" << endl;
-  list.insertLast(2); list.printList(); cout << "insertLast 2" << endl;
-  list.removeFront(); list.printList(); cout << "removeFront" << endl;
-  list.removeLast(); list.printList();  cout << "removeLast" << endl;
-  list.removeLast(); list.printList(); cout << "removeLast" << endl;
+	list.insertFront(0); list.printList(); cout << "insertFront 0" << endl;
+	list.insertLast(2); list.printList(); cout << "insertLast 2" << endl;
+  	//list.removeFront(); list.printList(); cout << "removeFront" << endl;
+  	//list.removeLast(); list.printList();  cout << "removeLast" << endl;
+  	//list.removeLast(); list.printList(); cout << "removeLast" << endl;
+  	//list.removeFront(); list.printList(); cout << "removeFront" << endl;
   
 }
 
