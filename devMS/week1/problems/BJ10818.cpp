5e2570611e7ff95b65ba5687f2f271d8b63d5d40
minsunny5 <minsunny5@gmail.com>
2021-03-08 17:24:50 +0900

    Baekjoon 3/7

---
+++ devMS/week1/problems/BJ10818.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int size;
+    int input;
+    int min = 10000000;
+    int max = -10000000;
+
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> input;
+
+        if (input < min)
+        {
+            min = input;
+        }
+        if (input > max)
+        {
+            max = input;
+        }
+    }
+    cout << min << " ";
+    cout << max;
+}
+
---
+++ devMS/week1/problems/BJ2562.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int input[9] = { 0, };
+    int max = -10000000;
+    int max_idx = 0;
+
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> input[i];
+
+        if (input[i] > max)
+        {
+            max = input[i];
+            max_idx = i+1;
+        }
+    }
+    cout << max << endl;
+    cout << max_idx;
+
+    return 0;
+}
