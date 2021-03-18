fefb6d2db9660f17163d5799f43d1fb5693455e4
minsunny5 <minsunny5@gmail.com>
2021-03-18 09:30:08 +0900

    Solved BJ and LeetCode problems

---
+++ devMS/week1/problems/BJ3052.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+void count(int* arr);
+
+int main()
+{
+    int remainder[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> remainder[i];
+        remainder[i] = remainder[i] % 42;
+    }
+
+    count(remainder);
+
+    return 0;
+}
+
+void count(int* arr)
+{
+    int num[42] = { 0, };//element N : number of N in the arr.
+    int result = 0;
+
+    for (int i = 0; i < 42; i++)//remainder could be 0 to 41
+    {
+        for (int j = 0; j < 10; j++)//number of input(remainder array) is 10
+        {
+            if (arr[j] == i) //if the element is 0 then num[0] is increased by 1
+            {
+                num[i] += 1;
+            }
+        }
+        if (num[i] != 0)//count the number of remainders which is not zero.
+        {
+            result++;
+        }       
+    }
+    cout << result << endl;
+}
+
---
+++ devMS/week1/problems/BJ4344.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int total_num;
+    cin >> total_num;
+
+    int student_num = 0;
+    int score[1000] = { 0, };
+    int avg = 0;
+    float result = 0;
+
+    for (int i = 0; i < total_num; i++)//per test case
+    {
+        int sum_result = 0;
+        int student_over = 0;
+        cin >> student_num;
+        for (int j = 0; j < student_num; j++)//per student score: get sum_result
+        {
+            cin >> score[j];
+            sum_result += score[j];
+        }
+        avg = sum_result / student_num;
+
+        for (int k = 0; k < student_num; k++)//per student score: get student_over
+        {
+            if (score[k] > avg)
+            {
+                student_over++;
+            }
+        }
+        result = (float)student_over / student_num;
+
+        cout << fixed;
+        cout.precision(3);
+        cout << result * 100 << "%" <<endl;
+    }
+    return 0;
+}
+
