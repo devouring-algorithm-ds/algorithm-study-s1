b8aa456b6d60814db28a3415863de1fc6953e8fa
ipflegen <ipflegen@pm.me>
2021-03-29 20:19:53 +0900

    binary tree implemented

---
+++ eubug/week5/implementation/binarytree/main.cpp
@@ -0,0 +1,28 @@
+#include "binarytree.hpp"
+
+int main() {
+  BinaryTree bt;
+
+  bt.insertNode('A');
+  bt.insertNode('B');
+  bt.insertNode('C');
+  bt.insertNode('D');
+  bt.insertNode('E');
+  bt.insertNode('F');
+  bt.insertNode('G');
+  bt.insertNode('H');
+  bt.insertNode('I');
+
+  cout << "Inorder: ";
+  bt.inorder(0);
+  cout << endl;
+
+  cout << "Preorder: ";
+  bt.preorder(0);
+  cout << endl;
+
+  cout << "Postorder: ";
+  bt.postorder(0);
+  cout << endl;
+  return 0;
+}
