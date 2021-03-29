8117f42e2f10fa513bc41ba0421a46ee87eaf365
minsunny5 <minsunny5@gmail.com>
2021-03-30 00:04:18 +0900

    Josephus problem solved by circular linked list

---
+++ devMS/week2/problems/BJ1158_요세푸스/circular_Josephus.cpp
@@ -0,0 +1,84 @@
+﻿/*
+* 백준: 요세푸스 문제 
+* https://www.acmicpc.net/problem/1158
+*/
+
+#include <iostream>
+#include "circular_linkedlist.h"
+
+using namespace std;
+
+Node<int>* kill_nth(int n, LinkedList<int>& list, Node<int>* searchNode);
+Node<int>* passDummy(LinkedList<int>& list, Node<int>* searchNode);
+
+int main()
+{
+    LinkedList<int> list;
+    Node<int>* searchNode;//Point to victim's next 
+
+    int total_num;
+    int kill_num;
+
+    //Input handling
+    cin >> total_num;
+    cin >> kill_num;
+
+    //Generating list from the input
+    for (int i = 1; i <= total_num; i++) {
+        list.push_back(i);
+    }
+
+    searchNode = list.head->next;
+    
+    cout << "<";
+    //Searching victim to kill
+    while (!list.empty())
+    {
+        searchNode = kill_nth(kill_num,list,searchNode);
+    }
+    cout << ">";
+
+    return 0;
+
+
+}
+/*
+* kill nth node
+*/
+Node<int>* kill_nth(int n, LinkedList<int>& list, Node<int>* searchNode)
+{
+    Node<int>* victimNode;// target that will be killed.
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        searchNode = (searchNode->next);
+        searchNode = passDummy(list, searchNode);
+    }
+    
+    searchNode = (searchNode->next);
+    victimNode = searchNode->prev;
+        
+    cout << victimNode -> data;
+    list.erase(victimNode);
+
+    if (!list.empty()) //list가 비어있으면 dummy만 남아있어서 passDummy를 돌리면 무한루프가 된다.
+    {
+        searchNode = passDummy(list, searchNode);
+        cout << ", ";
+    }
+  
+    return searchNode;
+    
+}
+
+/*
+* Pass Dummy part: head and tail node
+*/
+Node<int>* passDummy(LinkedList<int>& list, Node<int>* searchNode) 
+{
+    while ((searchNode == list.head) || (searchNode == list.tail))
+    {
+        searchNode = (searchNode->next);
+    }
+    return searchNode;
+}
---
+++ devMS/week2/problems/BJ1158_요세푸스/circular_linkedlist.h
@@ -0,0 +1,155 @@
+#ifndef CIRCULAR_LINKEDLIST_H
+#define CIRCULAR_LINKEDLIST_H
+
+template <typename T>
+class Node {
+
+public:
+    Node<T>* prev;
+    Node<T>* next;
+    T data;
+
+    Node(T val = T()) {
+        prev = nullptr;
+        next = nullptr;
+        data = val;
+    }
+};
+
+template <class T>
+class LinkedList {
+
+    // member variables
+    int sizeVal;
+    Node<T>* newNode;
+    Node<T>* frontNode;
+    Node<T>* backNode;
+
+
+public:
+
+    // Dummy nodes
+    Node<T>* head;
+    Node<T>* tail;
+
+
+    LinkedList() {
+        // Constructor.
+        head = new Node<T>;
+        tail = new Node<T>;
+
+        head->prev = tail;
+        head->next = tail;
+
+        tail->prev = head;
+        tail->next = head;
+
+        sizeVal = 0;
+
+        newNode = nullptr;
+        frontNode = nullptr;
+        backNode = nullptr;
+    }
+
+    ~LinkedList() {
+        // Destructor.
+        clear();
+        delete head;
+        delete tail;
+
+    }
+
+
+    int size() {
+        // Return the number of elements in list.
+        return sizeVal;
+    }
+
+    bool empty() {
+        // Return true if no elements are in list and false otherwise.
+        if (size() == 0)
+            return true;
+        else
+            return false;
+    }
+
+    T& front() {
+        // Return the front (first) element.
+        // return type is REFERENCE (not value) of the data (not Node).
+        frontNode = head->next;
+        return (frontNode->data);
+    }
+
+    T& back() {
+        // Return the back (last) element.
+        backNode = tail->prev;
+        return (backNode->data);
+    }
+
+    void insert(Node<T>* node_before, T val) {
+        // Create a node with val. Insert it after node_before.
+        // i.e. node_before -> new node -> (node_before->next)
+
+        newNode = new Node<T>;
+        newNode->data = val;
+
+        newNode->prev = node_before;
+        newNode->next = node_before->next;
+        node_before->next->prev = newNode;
+        node_before->next = newNode;
+
+        sizeVal += 1;
+    }
+
+    void erase(Node<T>* target) {
+        // Remove target node from the list.
+
+        if (size() == 0)
+        {
+            std::cout << "Can't Remove: List is empty" << std::endl;
+            return;
+        }
+
+        target->next->prev = target->prev;
+        target->prev->next = target->next;
+        delete target;
+
+        sizeVal -= 1;
+    }
+
+    void push_front(T val) {
+        // Create a node with val. Add it to the front of the list.
+        insert(head, val);
+    }
+
+    void pop_front() {
+        // Remove the front node.
+        frontNode = head->next;
+        erase(frontNode);
+    }
+
+    void push_back(T val) {
+        // Create a node with val. Add it to the back of the list.
+        backNode = tail->prev;
+        insert(backNode, val);
+    }
+
+    void pop_back() {
+        // Remove the back node.
+        backNode = tail->prev;
+        erase(backNode);
+    }
+
+    void clear() {
+        // Remove all nodes in list.
+        for (int i = 0; i < sizeVal; i++)
+        {
+            pop_front();
+        }
+    }
+
+    
+
+    
+};
+#endif
