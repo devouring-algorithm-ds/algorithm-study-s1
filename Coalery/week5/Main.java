3bccdd6be7245ae8641eeb94c0936c8cc9891b90
러리 <doralife12@naver.com>
2021-04-05 17:33:31 +0900

    Feat: Binary Tree Implementation

---
+++ Coalery/week5/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+	public static void main(String[] args) {
+		BinaryTree<Integer> btree = new BinaryTree<Integer>();
+		
+		btree.insertNode(1);
+		btree.insertNode(2);
+		btree.insertNode(3);
+		btree.insertNode(4);
+		btree.insertNode(5);
+		btree.insertNode(6);
+		btree.insertNode(7);
+		btree.insertNode(8);
+		btree.insertNode(9);
+		btree.insertNode(10);
+		btree.insertNode(11);
+		btree.insertNode(12);
+		btree.insertNode(13);
+		btree.insertNode(14);
+		btree.insertNode(15);
+	}
+}
