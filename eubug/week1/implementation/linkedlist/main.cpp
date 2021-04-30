253d29a41b5ded5c7a3557847028624c21688eff
ipflegen <ipflegen@pm.me>
2021-04-30 16:47:43 +0900

    update to current

--- eubug/week1/implementation/linkedlist/main.cpp
+++ eubug/week1/implementation/linkedlist/main.cpp
@@ -89,66 +89,6 @@ int main()
   list.print();
   cout << "removeAt(1): " << list.removeAt(1) << endl;
   list.print();
-  cout << endl;
-
-  cout << "InsertAt(100, 1) -> insert 100 at index 1" << endl;
-  list.insertAt(100, 1);
-  list.print();
-
-  cout << "InsertAt(200, 0) -> insert 200 at index 0" << endl;
-  list.insertAt(200, 0);
-  list.print();
-  cout << endl;
-
-  cout << "while the list is not empty ... " << endl;
-  while(!list.isEmpty()) 
-    cout << "removeFirst -> Removed " << list.removeFirst() << endl;
-  list.print();
-  cout << endl;
-
-  cout << "InsertAt(1, 10) -> insert 1 at index 10" << endl;
-  list.insertAt(1, 10);
-  list.print();
-  cout << "InsertAt(2, 10) -> insert 2 at index 10" << endl;
-  list.insertAt(2, 10);
-  list.print();
-  cout << "InsertAt(3, 0) -> insert 3 at index 0" << endl;
-  list.insertAt(3, 0);
-  list.print();
-  cout << "InsertAt(4, -1) -> insert 4 at index -1" << endl;
-  list.insertAt(4, -1);
-  list.print();
-  cout << endl;
-
-  cout << "removeAt(1): " << list.removeAt(1) << endl;
-  list.print();
-  cout << "removeAt(1): " << list.removeAt(1) << endl;
-  list.print();
-  cout << "removeAt(5): " << list.removeAt(5) << endl;
-  list.print();
-  cout << "removeAt(-1): " << list.removeAt(-1) << endl;
-  list.print();
-  cout << endl;
-
-
-  cout << "insertFront(100)" << endl;
-  list.insertFront(100);
-  list.print();
-
-  cout << "insertBack(200)" << endl;
-  list.insertBack(200);
-  list.print();
-
-  cout << "InsertAt(300, 1)" << endl;
-  list.insertAt(300, 1);
-  list.print();
-
-  cout << "removeFirst -> Removed " << list.removeFirst() << endl;
-  list.print();
-  cout << "removeLast -> Removed " << list.removeLast() << endl;
-  list.print();
-  cout << "removeAt(1): " << list.removeAt(1) << endl;
-  list.print();
 
   return 0;
 }
--- eubug/week6/implementation/bst/main.cpp
+++ eubug/week6/implementation/bst/main.cpp
@@ -8,6 +8,14 @@ int main() {
   BST bst;
 
 #if 0
+  bst.insert(50);
+  bst.insert(30);
+  bst.insert(70);
+  bst.insert(20);
+  bst.insert(40);
+  bst.insert(60);
+  bst.insert(80);
+  bst.inorder();
 
   bst.remove(20);
   bst.inorder();
@@ -29,33 +37,7 @@ int main() {
   bst.insert(18);
   cout << "search for 18: " << bst.search(18) << endl;
   bst.insert(25);
-=======
-
-  bst.remove(20);
   bst.inorder();
-  bst.remove(30);
-  bst.inorder();
-  bst.remove(50);
->>>>>>> 6387bf6... BST implemented
-  bst.inorder();
-#endif
-
-  bst.insert(7);
-  bst.insert(6);
-  cout << "search for 7: " << bst.search(7) << endl;
-  cout << "search for 10: " << bst.search(10) << endl;
-  bst.insert(15);
-  bst.insert(10);
-  bst.insert(17);
-  bst.insert(16);
-  bst.insert(19);
-  bst.insert(18);
-  cout << "search for 18: " << bst.search(18) << endl;
-  bst.insert(25);
-  bst.inorder();
-
-  bst.remove(15);
-  bst.inorder(); 
 
   bst.remove(15);
   bst.inorder(); 
